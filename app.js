3afaa38fe58f8afd5cc3423028762384e08cfbb6
Alejandro Cruz <acruz.jobs@gmail.com>
2025-03-15 16:34:27 -0300

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Variables.
+let amigos = [];
+let entradaNombre = "";
+
+const patronValidacion = /^[A-Za-z\s]+$/;
+
+//Funciones.
+function agregarAmigo() {
+    //Capturar el valor de entrada en arrray amigos.
+    entradaNombre = document.getElementById('amigo').value;
+
+    //Validar entrada.
+    if (entradaNombre == '') { //Campo vacío.
+        alert('Por favor, inserte un nombre.');
+        return false;
+    } else if (!patronValidacion.test(entradaNombre)){ //Caracteres extraños.
+        alert('Solo se admiten letras');
+        limpiarEntrada('amigo',"");
+        return false;
+    } else {
+        //Actualizar array amigos y limpiar campo.
+        amigos.push(entradaNombre);
+        limpiarEntrada('amigo',"");
+    }
+    return console.log(amigos);
+}
+
+function limpiarEntrada(id,valor){
+    let elementoHTML = document.getElementById(id);
+    elementoHTML.value = valor;
+    return;
+}
